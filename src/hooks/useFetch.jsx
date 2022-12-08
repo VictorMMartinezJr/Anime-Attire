a45a08a7903d683bf31d07f8456d5bf624f877f1
Victor Martinez Jr <victormtzcodes@gmail.com>
2022-12-08 02:25:32 -0500

    changed useFetch hook to async await and added try catch block

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -2,14 +2,26 @@ import { useEffect, useState } from "react";
 
 const useFetch = (url) => {
   const [products, setProducts] = useState([]);
+  const [error, setError] = useState(false);
+
+  const fetchData = async (url) => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setProducts(data);
+      if (error) {
+        setError(false);
+      }
+    } catch (err) {
+      setError(err.message);
+    }
+  };
 
   useEffect(() => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => setProducts(data));
+    fetchData(url);
   }, []);
 
-  return { products };
+  return { products, error };
 };
 
 export default useFetch;
